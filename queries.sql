0749b8129db4f1faff29afd199c002888889ad89
Your Name <you@example.com>
2022-09-20 16:59:26 -0600

    w18b

---
+++ queries.sql
@@ -0,0 +1,60 @@
+insert into customer (username, password, created_at) values
+('user_one', 'pass', '2022-01-01'),
+('user_two', 'pass_two', '2022-01-01'),
+('user_three', 'pass_three', '2022-01-01'),
+('user_four', 'pass_four', '2022-01-01'),
+('user_five', 'pass_five', '2022-01-01');
+
+insert into item (name, price) values 
+('item_one', 10),
+('item_two', 20),
+('item_three', 30),
+('item_four', 40),
+('item_five', 50),
+('item_six', 60),
+('item_seven', 70),
+('item_eight', 80),
+('item_nine', 90),
+('item_ten', 100);
+
+insert into purchase (purchase_time, customer_id, item_id) values
+('2022-01-20 11:00:00', 1, 1),
+('2022-01-20 11:00:00', 1, 2),
+('2022-01-20 11:00:00', 1, 6),
+('2022-01-20 11:00:00', 1, 9),
+('2022-01-20 11:00:00', 1, 10),
+('2022-01-20 09:00:00', 2, 3),
+('2022-01-20 09:00:00', 2, 5),
+('2022-01-20 13:00:00', 3, 7),
+('2022-01-20 17:00:00', 4, 1),
+('2022-01-20 17:00:00', 4, 1),
+('2022-01-20 17:00:00', 4, 3),
+('2022-01-20 17:00:00', 4, 6),
+('2022-01-20 13:30:00', 4, 8),
+('2022-01-20 10:00:00', 4, 10),
+('2022-01-20 12:00:00', 5, 1),
+('2022-01-20 12:00:00', 5, 4),
+('2022-01-20 12:00:00', 5, 9),
+('2022-01-20 12:00:00', 5, 3),
+('2022-01-20 11:30:00', 1, 2),
+('2022-01-20 09:25:00', 1, 1);
+
+select c.username, i.name, p.purchase_time 
+from customer c inner join purchase p on p.customer_id = c.id
+inner join item i on i.id=p.item_id 
+order by p.purchase_time  desc
+limit 5;
+
+
+select c.username, count(c.id)
+from customer c inner join purchase p on p.customer_id = c.id
+inner join item i on i.id=p.item_id 
+group by c.id;
+
+call five_most_recent
+
+call specific_customer_purchases(2);
+
+call specific_customer_purchases(1);
+
+call specific_customer_purchases(3);
---
+++ wk18warmup_dbfunctions.sql
@@ -0,0 +1,159 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: w18b
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `password` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `customer_un` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customer`
+--
+
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+INSERT INTO `customer` VALUES (1,'user_one','pass','2022-01-01'),(2,'user_two','pass_two','2022-01-01'),(3,'user_three','pass_three','2022-01-01'),(4,'user_four','pass_four','2022-01-01'),(5,'user_five','pass_five','2022-01-01');
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8mb4_bin DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'item_one',10),(2,'item_two',20),(3,'item_three',30),(4,'item_four',40),(5,'item_five',50),(6,'item_six',60),(7,'item_seven',70),(8,'item_eight',80),(9,'item_nine',90),(10,'item_ten',100);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `purchase`
+--
+
+DROP TABLE IF EXISTS `purchase`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `purchase` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `purchase_time` datetime DEFAULT NULL,
+  `customer_id` int(10) unsigned DEFAULT NULL,
+  `item_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `purchase_FK` (`customer_id`),
+  KEY `purchase_FK_1` (`item_id`),
+  CONSTRAINT `purchase_FK` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `purchase_FK_1` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase`
+--
+
+LOCK TABLES `purchase` WRITE;
+/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
+INSERT INTO `purchase` VALUES (1,'2022-01-20 11:00:00',1,1),(2,'2022-01-20 11:00:00',1,2),(3,'2022-01-20 11:00:00',1,6),(4,'2022-01-20 11:00:00',1,9),(5,'2022-01-20 11:00:00',1,10),(6,'2022-01-20 09:00:00',2,3),(7,'2022-01-20 09:00:00',2,5),(8,'2022-01-20 13:00:00',3,7),(9,'2022-01-20 17:00:00',4,1),(10,'2022-01-20 17:00:00',4,1),(11,'2022-01-20 17:00:00',4,3),(12,'2022-01-20 17:00:00',4,6),(13,'2022-01-20 13:30:00',4,8),(14,'2022-01-20 10:00:00',4,10),(15,'2022-01-20 12:00:00',5,1),(16,'2022-01-20 12:00:00',5,4),(17,'2022-01-20 12:00:00',5,9),(18,'2022-01-20 12:00:00',5,3),(19,'2022-01-20 11:30:00',1,2),(20,'2022-01-20 09:25:00',1,1);
+/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'w18b'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `five_most_recent` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `five_most_recent`()
+begin
+select c.username, i.name, p.purchase_time 
+from customer c inner join purchase p on p.customer_id = c.id
+inner join item i on i.id=p.item_id 
+order by p.purchase_time  desc
+limit 5;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `specific_customer_purchases` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `specific_customer_purchases`(customer_id_input int unsigned)
+begin
+select c.username, count(c.id)
+from customer c inner join purchase p on p.customer_id = c.id
+inner join item i on i.id=p.item_id 
+where c.id =customer_id_input
+group by c.id;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-20 16:58:35
